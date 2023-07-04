e1568e585cf0514205c87e1ee799be79e1c9e904
umar006 <umaralfaruq597@gmail.com>
2023-07-04 10:51:34 +0700

    Create dynamic grid generator

--- odin-etch-a-skecth/script.js
+++ odin-etch-a-skecth/script.js
@@ -1,12 +1,25 @@
-const grids = [];
+const gridAmount = document.querySelector("input[name=grid-amount]");
+const changeGrid = document.querySelector("button[name=change-grid]");
+changeGrid.addEventListener("click", generateGrid);
 
-for(let i = 0; i < 16; i++) {
+function generateGrid() {
+  const grids = [];
+  const amount = Number(gridAmount.value);
+
+  for (let i = 0; i < amount ** 2; i++) {
     const grid = document.createElement("div");
     grid.setAttribute("id", i + 1);
-    grid.style.width = "200px";
-    grid.style.height = "200px";
+    grid.style.width = 800 / amount + "px";
+    grid.style.height = 800 / amount + "px";
+    grid.style.border = "1px solid black";
     grids.push(grid);
-}
+  }
 
-const container = document.querySelector(".container");
-container.append(...grids);
+  const container = document.querySelector(".container");
+
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+
+  container.append(...grids);
+}
