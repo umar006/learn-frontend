173f989fb24e2e6d8b434f0fe5f0ad0dca8ac49d
umar006 <umaralfaruq597@gmail.com>
2023-07-05 14:27:06 +0700

    Calculate number based on provided operator

--- odin-calculator/script.js
+++ odin-calculator/script.js
@@ -1,3 +1,18 @@
+function operate(firstNumber, secondNumber, operator) {
+  switch (operator) {
+    case "+":
+      return add(firstNumber, secondNumber);
+    case "-":
+      return substract(firstNumber, secondNumber);
+    case "*":
+      return multiply(firstNumber, secondNumber);
+    case "/":
+      return divide(firstNumber, secondNumber);
+    default:
+      return "Wrong operator";
+  }
+}
+
 function add(a, b) {
   return a + b;
 }
