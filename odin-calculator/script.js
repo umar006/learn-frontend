2c88eab38b656ad13c20eb86f6b27cbac7d91876
umar006 <umaralfaruq597@gmail.com>
2023-07-07 17:26:19 +0700

    Implement simple shunting yard algorithm

--- odin-calculator/script.js
+++ odin-calculator/script.js
@@ -1,16 +1,65 @@
+const operators = ["+", "-", "*", "/"];
+const operatorPrecedence = {
+  "+": 2,
+  "-": 2,
+  "*": 3,
+  "/": 3,
+};
+
 const field = document.querySelector("#container input");
 const numpad = document.querySelectorAll("#numpad button");
 numpad.forEach((numpad) => {
   numpad.addEventListener("click", function inputValue(e) {
     let value = e.target.textContent;
 
+    if (operators.includes(value)) value = ` ${value} `;
+
+    if (value === "=") {
+      const decode = field.value.split(" ");
+      const outputQueue = [];
+      const operatorStack = [];
+
+      // ref1: https://en.wikipedia.org/wiki/Shunting_yard_algorithm
+      // ref2: https://brilliant.org/wiki/shunting-yard-algorithm
+      while (decode.length) {
+        const token = decode.shift();
+
+        if (!isNaN(token)) {
+          outputQueue.push(Number(token));
+        } else if (operators.includes(token)) {
+          while (
+            operatorStack.length &&
+            operatorPrecedence[token] <=
+              operatorPrecedence[operatorStack[operatorStack.length - 1]]
+          ) {
+            const secondNumber = outputQueue.pop();
+            const firstNumber = outputQueue.pop();
+            const total = operate(
+              firstNumber,
+              secondNumber,
+              operatorStack.pop()
+            );
+            outputQueue.push(total);
+          }
+          operatorStack.push(token);
+        }
+
+        while (!decode.length && operatorStack.length) {
+          const secondNumber = outputQueue.pop();
+          const firstNumber = outputQueue.pop();
+          const total = operate(firstNumber, secondNumber, operatorStack.pop());
+          outputQueue.push(total);
+        }
+      }
+
+      field.value = outputQueue[0];
+      return;
+    }
+
     if (value === "clear") {
       field.value = "";
       return;
     }
-
-    const operators = ["+", "-", "*", "/"];
-    if (operators.includes(value)) value = ` ${value} `;
 
     field.value += value;
   });
